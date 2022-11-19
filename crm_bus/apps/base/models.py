b493d0e0d1039e1ebc2e7ee07f6314a82ef3c05f
Jorge Fandino <beto3971@gmail.com>
2022-11-19 02:52:02 -0400

    feat: create a new app base to inherated the common models

---
+++ crm_bus/apps/base/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from simple_history.models import HistoricalRecords
+
+
+class BaseModel(models.Model):
+    """model base to be inherited for other models with dis params"""
+    id = models.AutoField(primary_key=True)
+    state = models.BooleanField(default=True)
+    created_date = models.DateField(auto_now=False, auto_now_add=True)
+    modified_date = models.DateField(auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField(auto_now=True, auto_now_add=False)
+    historical = HistoricalRecords(user_model="users.User", inherit=True)
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+
+    class Meta:
+        """Meta definition for BaseModel."""
+        abstract = True
+        verbose_name = 'Model Base'
+        verbose_name_plural = 'Models Base'
