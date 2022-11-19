f1baf5226d291bc9493411a0a0374e58aaabc4b9
Jorge Fandino <beto3971@gmail.com>
2022-11-19 19:37:51 -0400

    feat: refator UserViewSet with inherit of BaseGenericViewSet

--- crm_bus/apps/users/api/api.py
+++ crm_bus/apps/users/api/api.py
@@ -1,59 +1,26 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import viewsets, status
+from rest_framework import status
 
-from apps.users.models import User
 from apps.users.api.serializers import UserSerializer, UpdateUserSerializer, ListUserSerializer
+from apps.base.api.generic_api import BaseGenericViewSet
 
 
-class UserViewSet(viewsets.GenericViewSet):
-    model = User
+class UserViewSet(BaseGenericViewSet):
     serializer_class = UserSerializer
     list_serializer_class = ListUserSerializer
+    update_serializer_class = UpdateUserSerializer
+    model_object = 'User'
     queryset = None
-
-    def get_object(self, pk):
-        return get_object_or_404(self.model, pk=pk)
 
     def get_queryset(self):
         if self.queryset is None:
-            self.queryset = self.model.objects.filter(is_active=True).values('id', 'username', 'email', 'name')
+            model = self.get_serializer().Meta.model
+            self.queryset = model.objects.filter(is_active=True).values('id', 'username', 'email', 'name')
         return self.queryset
 
-    def list(self, request):
-        users = self.get_queryset()
-        users_serializer = self.list_serializer_class(users, many=True)
-        return Response(users_serializer.data, status=status.HTTP_200_OK)
-
-    def create(self, request):
-        user_serializer = self.serializer_class(data=request.data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return Response({'message': 'User save success.'}, status=status.HTTP_200_OK)
-        return Response({'message': 'Have error/s in the form',
-                         'errors': user_serializer.errors},
-                        status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        user = self.get_object(pk)
-        user_serializer = self.serializer_class(user)
-        return Response(user_serializer.data)
-
-    def update(self, request, pk=None):
-        user = self.get_object(pk)
-        user_serializer = UpdateUserSerializer(user, data=request.data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return Response({
-                'message': 'user updated success'
-            }, status=status.HTTP_200_OK)
-        return Response({
-            'message': ' Error in update user',
-            'errors': user_serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
-
     def destroy(self, request, pk=None):
-        user_destroy = self.model.objects.filter(id=pk).update(is_active=False)
+        model = self.get_serializer().Meta.model
+        user_destroy = model.objects.filter(id=pk).update(is_active=False)
         if user_destroy == 1:
             return Response({
                 'message': 'user deleted'
