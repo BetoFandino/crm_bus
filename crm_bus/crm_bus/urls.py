68f520fa01df3d42d5b5c0c1a1d10de88e5a3a2d
Jorge Fandino <beto3971@gmail.com>
2022-11-19 21:09:03 -0400

    feat: add new apps to url

--- crm_bus/crm_bus/urls.py
+++ crm_bus/crm_bus/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.api.routers')),
-    path('employees/', include('apps.employees.api.routers'))
+    path('employees/', include('apps.employees.api.routers')),
+    path('transports_units/', include('apps.transport_units.api.routers'))
 ]
